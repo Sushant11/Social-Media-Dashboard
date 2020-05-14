29566aa1141dafd2932ad5da068126bbde1e43b8
sushantchitrakar@gmail.com <sushantchitrakar@gmail.com>
2020-05-14 19:31:13 +0545

    resp

--- toggle.js
+++ toggle.js
@@ -4,8 +4,8 @@ const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = false;
+    if (currentTheme === 'light') {
+        toggleSwitch.checked = true;
     }
 }
 
